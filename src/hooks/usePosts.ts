3cd5fbe6e2186e34a1c8f14b0f1a3f696255cb4f
TomohiroTani28 <amlinks0621@gmail.com>
2025-04-01 21:42:18 +0900

    Update usePosts.ts

--- src/hooks/usePosts.ts
+++ src/hooks/usePosts.ts
@@ -1,6 +1,6 @@
 // src/hooks/usePosts.ts
 import { useQuery, useSubscription } from '@apollo/client';
-import { GET_POSTS, ON_NEW_POSTS } from '@/app/lib/graphql/queries/post';
+import { GET_POSTS, ON_NEW_POSTS } from '@/lib/queries/post';
 
 export function usePosts(limit: number = 20, offset: number = 0) {
   const { data, loading, error } = useQuery(GET_POSTS, {
