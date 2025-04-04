98c89d69244d101614262675f10d09f1dd28fd0c
TomohiroTani28 <amlinks0621@gmail.com>
2025-04-04 15:27:53 +0900

    Update ApolloWrapper.tsx

--- src/app/ApolloWrapper.tsx
+++ src/app/ApolloWrapper.tsx
@@ -1,11 +1,9 @@
 // src/app/ApolloWrapper.tsx
-import dynamic from "next/dynamic";
+import ApolloClientWrapper from "./ApolloClientWrapper";
 
 interface ApolloWrapperProps {
   readonly children: React.ReactNode;
 }
-
-const ApolloClientWrapper = dynamic(() => import("./ApolloClientWrapper"), { ssr: false });
 
 export default function ApolloWrapper({ children }: ApolloWrapperProps) {
   return <ApolloClientWrapper>{children}</ApolloClientWrapper>;
