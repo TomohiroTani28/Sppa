391f9e8a1ede769efdbdf742a4fb8f033945f632
TomohiroTani28 <amlinks0621@gmail.com>
2025-04-06 20:20:58 +0900

    Delete page.tsx

--- src/app/test-session/page.tsx
+++
@@ -1,8 +0,0 @@
-// src/app/test-session/page.tsx
-import { getServerSession } from "next-auth";
-import { authOptions } from "../api/auth/[...nextauth]/route";
-
-export default async function TestSessionPage() {
-  const session = await getServerSession(authOptions);
-  return <pre>{JSON.stringify(session, null, 2)}</pre>;
-}
