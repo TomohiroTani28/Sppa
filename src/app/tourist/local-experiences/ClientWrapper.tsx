0f0951e99f24ffad7396f5603e3c9e6856d15cc1
TomohiroTani28 <amlinks0621@gmail.com>
2025-04-07 21:33:28 +0900

    e

---
+++ src/app/tourist/local-experiences/ClientWrapper.tsx
@@ -0,0 +1,7 @@
+// src/app/tourist/local-experiences/ClientWrapper.tsx
+"use client";
+import LocalExperiencesClient from "./LocalExperiencesClient";
+
+export default function ClientWrapper() {
+  return <LocalExperiencesClient />;
+}
--- src/app/tourist/local-experiences/page.tsx
+++ src/app/tourist/local-experiences/page.tsx
@@ -1,20 +1,14 @@
 // src/app/tourist/local-experiences/page.tsx
 import { Suspense } from "react";
-import dynamic from "next/dynamic";
+import ClientWrapper from "@/app/tourist/local-experiences/ClientWrapper";
 
 // サーバーコンポーネントでのrevalidate設定
 export const revalidate = 0;
 
-// クライアントコンポーネントの動的インポート
-const LocalExperiencesClient = dynamic(() => import("./LocalExperiencesClient"), {
-  ssr: false,
-  loading: () => <div>Loading...</div>
-});
-
 export default function LocalExperiencesPage() {
   return (
     <Suspense fallback={<div>Loading...</div>}>
-      <LocalExperiencesClient />
+      <ClientWrapper />
     </Suspense>
   );
 }
