4796cad2a0aaf6794f1be062b839a8992843f43d
TomohiroTani28 <amlinks0621@gmail.com>
2025-04-07 22:49:24 +0900

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@
 import React from "react";
 import { SessionProvider } from "next-auth/react";
 import { Providers } from "./providers";
+import "src/styles/globals.css";
 
 export default function RootLayout({
   children,
