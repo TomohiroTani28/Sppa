d6f7a55fbf604504044032f1f0e5a5cb4e4674b6
TomohiroTani28 <amlinks0621@gmail.com>
2025-04-07 20:51:11 +0900

    Update _not-found.tsx

--- src/app/_not-found.tsx
+++ src/app/_not-found.tsx
@@ -1,7 +1,7 @@
 // src/app/_not-found.tsx
+"use client";
 export default function NotFound() {
   return <h1>404 - Page Not Found</h1>;
 }
 
-// Explicitly force static generation (optional, as it’s the default for server components)
 export const dynamic = "force-static";
