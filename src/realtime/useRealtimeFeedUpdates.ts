df67d31453bd11fb542c9db1837317c88e592302
TomohiroTani28 <amlinks0621@gmail.com>
2025-04-11 21:18:24 +0900

    e

--- src/realtime/useRealtimeFeedUpdates.ts
+++ src/realtime/useRealtimeFeedUpdates.ts
@@ -122,6 +122,7 @@ export const useRealtimeFeedUpdates = (
     context: {
       headers: {
         'x-hasura-role': role,
+        'x-hasura-admin-secret': process.env.NEXT_PUBLIC_HASURA_GRAPHQL_ADMIN_SECRET || '',
       },
     },
   });
